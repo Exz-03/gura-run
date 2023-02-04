ebee5bf3ebae723c318feb97619dbf638453c8c6
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2023-02-04 14:42:33 +0700

    Update conn.js

--- conn.js
+++ conn.js
@@ -5039,7 +5039,7 @@ conn.sendMessage(from, {video:{url:i.url}, caption:`Type : ${i.type}`, mimetype:
           reply(mess.wait)
           try {
             var configuration = new Configuration({
-              apiKey: "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",
+              apiKey: "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",
             });
             let openai = new OpenAIApi(configuration);
             let response = await openai.createCompletion({
@@ -5064,7 +5064,7 @@ conn.sendMessage(from, {video:{url:i.url}, caption:`Type : ${i.type}`, mimetype:
           if (!q) return reply(`Mencari gambar/foto dari Ai.\n\nContoh:\n${prefix}${command} gunung Bromo `)
           try {
             let configuration = new Configuration({
-              apiKey: "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",
+              apiKey: "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",
             });
             let openai = new OpenAIApi(configuration);
             let response = await openai.createImage({
